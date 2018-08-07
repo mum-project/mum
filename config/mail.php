858a13b01b70ed538c00d1b26cf465e763016753
Martin Bock <mail@mnbo.de>
2018-08-07 14:23:43 +0200

    Update .env.example to include MAIL_FROM_ADDRESS and _NAME

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS'),
+        'address' => env('MAIL_FROM_ADDRESS', env('MAIL_USERNAME')),
         'name' => env('MAIL_FROM_NAME', config('app.name')),
     ],
 
